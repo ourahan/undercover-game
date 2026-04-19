b4ecd7d8468f0d166ca7884e1359b9696e8b9d9d
ourahan <johnsete347@gmail.com>
2026-04-19 17:33:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -204,5 +204,5 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => console.log(`Serveur sur port ${PORT}`));
